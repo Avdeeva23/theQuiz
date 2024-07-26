bddfc335d829bc8b30f1a1b5e7c22fa490c07418
IgorPetrov-13 <petrov_igor_rf@mail.ru>
2024-07-26 09:38:47 +0300

    feat/questions

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,28 @@
-import { useState } from 'react'
-import './App.css'
-import Home from './components/home/Home'
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./components/home/Home";
+import ErrorPage from "./components/Error/ErrorPage";
+import ThemOne from "./components/ThemOne/ThemOne";
+import Quiz from "./components/ThemTwo/ThemTwo";
+import Navbar from "./components/Navbar/Navbar"
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-     <Home/>
-    </>
-  )
+    <BrowserRouter>
+    
+      <Navbar/>
+      <section className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/themone" element={<ThemOne />} />
+          <Route path="/themtwo" element={<Quiz />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </section>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/components/ThemOne/ThemOne.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import axios from "axios";
+
+function ThemOne() {
+
+const ThemOne = async ()=> {
+
+    const questions = axios.get(`/themone/${id}`)
+}
+
+  return (
+    <>
+      <div>ThemOne</div>
+    </>
+  );
+}
+
+export default ThemOne;
